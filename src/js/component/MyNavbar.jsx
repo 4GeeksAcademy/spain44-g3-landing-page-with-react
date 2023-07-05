36132ce21d4a72e9304d946553d63600012b5696
Yúliya Bundur <123424819+yuliyabundur@users.noreply.github.com>
2023-07-05 18:55:11 +0000

    Componente Navbar modificado

--- src/js/component/MyNavbar.jsx
+++ src/js/component/MyNavbar.jsx
@@ -2,7 +2,30 @@ import React from "react";
 
 export const MyNavbar = () => {
     return (
-    <h1>Hello</h1>
+        <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="https://getbootstrap.com">Start Bootstrap</a>
+          <button class="navbar-toggler justify-content-end" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
+            <ul class="navbar-nav">
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#">Home</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">About</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link disabled">Services</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Contact</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
     )
 }
 
