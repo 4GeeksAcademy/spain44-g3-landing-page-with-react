6d534a61e54c1e669c2c4820bfdd2dadca4cb15e
Yúliya Bundur <123424819+yuliyabundur@users.noreply.github.com>
2023-07-05 20:54:41 +0000

    Componente Footer creado

---
+++ src/js/component/MyFooter.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+
+export const MyFooter = () => {
+    return (
+        <div class="container">
+  <footer class="footer py-3 my-4">
+    <p class="nav-item"><a href="#" class="nav-link px-2 text-muted">Cohorte-44-Spain</a></p>
+    <ul class="nav justify-content-center border-bottom pb-3 mb-3">
+      <li class="nav-item"><a href="https://www.linkedin.com/in/yuliya-bundur/" class="nav-link px-2 text-muted">Yuliya Bundur</a></li>
+      <li class="nav-item"><a href="https://www.linkedin.com/in/carlos-duarte-rodr%C3%ADguez-bb2678174/" class="nav-link px-2 text-muted">Carlos Duarte</a></li>
+      <li class="nav-item"><a href="https://www.linkedin.com/in/hector-chocobar/" class="nav-link px-2 text-muted">Héctor Chocobar</a></li>
+      <li class="nav-item"><a href="https://www.linkedin.com/in/jrdelriod/" class="nav-link px-2 text-muted">José Del Río</a></li>
+      
+    </ul>
+        <p class="bottom-item text-center text-muted">
+          <a href="https://4geeksacademy.com/" className="buttom-link px-2 text-muted">© 4Geeks Academy</a>
+        </p>
+  </footer>
+</div>
+    )
+}
