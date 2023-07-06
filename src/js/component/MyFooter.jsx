a05f025a2d5e1043ffceeabb8b432a1735ec209e
Yúliya Bundur <123424819+yuliyabundur@users.noreply.github.com>
2023-07-06 18:10:30 +0000

    Navbar y Footer updated

--- src/js/component/MyFooter.jsx
+++ src/js/component/MyFooter.jsx
@@ -4,17 +4,18 @@ import React from "react";
 export const MyFooter = () => {
     return (
         <div class="container">
-  <footer class="footer py-3 my-4">
-    <p class="nav-item"><a href="#" class="nav-link px-2 text-muted">Cohorte-44-Spain</a></p>
-    <ul class="nav justify-content-center border-bottom pb-3 mb-3">
-      <li class="nav-item"><a href="https://www.linkedin.com/in/yuliya-bundur/" class="nav-link px-2 text-muted">Yuliya Bundur</a></li>
-      <li class="nav-item"><a href="https://www.linkedin.com/in/carlos-duarte-rodr%C3%ADguez-bb2678174/" class="nav-link px-2 text-muted">Carlos Duarte</a></li>
-      <li class="nav-item"><a href="https://www.linkedin.com/in/hector-chocobar/" class="nav-link px-2 text-muted">Héctor Chocobar</a></li>
-      <li class="nav-item"><a href="https://www.linkedin.com/in/jrdelriod/" class="nav-link px-2 text-muted">José Del Río</a></li>
+  <footer class="footer py-2 my-2">
+    <p class="nav-item"><a href="#" class="nav-link px-1 text-white">Cohorte-44-Spain</a></p>
+    <ul class="nav justify-content-center border-bottom pb-2 mb-3">
+      <li class="nav-item"><a href="https://www.linkedin.com/in/yuliya-bundur/" class="nav-link px-2 text-white">Yuliya Bundur</a></li>
+      <li class="nav-item"><a href="https://www.linkedin.com/in/carlos-duarte-rodr%C3%ADguez-bb2678174/" class="nav-link px-2 text-white">Carlos Duarte</a></li>
+      <li class="nav-item"><a href="https://www.linkedin.com/in/carlos-duarte-rodr%C3%ADguez-bb2678174/" class="nav-link px-2 text-white">Jorge Muñoz</a></li>
+      <li class="nav-item"><a href="https://www.linkedin.com/in/hector-chocobar/" class="nav-link px-2 text-white">Héctor Chocobar</a></li>
+      <li class="nav-item"><a href="https://www.linkedin.com/in/jrdelriod/" class="nav-link px-2 text-white">José Del Río</a></li>
       
     </ul>
-        <p class="bottom-item text-center text-muted">
-          <a href="https://4geeksacademy.com/" className="buttom-link px-2 text-muted">© 4Geeks Academy</a>
+        <p class="bottom-item text-center text-white">
+          <a href="https://4geeksacademy.com/" className="buttom-link px-1 text-white">© 4Geeks Academy</a>
         </p>
   </footer>
 </div>
