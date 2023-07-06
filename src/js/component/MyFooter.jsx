264c037c152d206bcae9133547a10eae33958354
Yúliya Bundur <123424819+yuliyabundur@users.noreply.github.com>
2023-07-06 20:01:21 +0000

    Footer updated

--- src/js/component/MyFooter.jsx
+++ src/js/component/MyFooter.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import img from "/workspaces/spain44-g3-landing-page-with-react/src/img/GitHub-Logo.png";
 
 
 export const MyFooter = () => {
@@ -6,12 +7,32 @@ export const MyFooter = () => {
         <div class="container">
   <footer class="footer py-2 my-2">
     <p class="nav-item"><a href="#" class="nav-link px-1 text-white">Cohorte-44-Spain</a></p>
-    <ul class="nav justify-content-center border-bottom pb-2 mb-3">
-      <li class="nav-item"><a href="https://www.linkedin.com/in/yuliya-bundur/" class="nav-link px-2 text-white">Yuliya Bundur</a></li>
-      <li class="nav-item"><a href="https://www.linkedin.com/in/carlos-duarte-rodr%C3%ADguez-bb2678174/" class="nav-link px-2 text-white">Carlos Duarte</a></li>
-      <li class="nav-item"><a href="https://www.linkedin.com/in/carlos-duarte-rodr%C3%ADguez-bb2678174/" class="nav-link px-2 text-white">Jorge Muñoz</a></li>
-      <li class="nav-item"><a href="https://www.linkedin.com/in/hector-chocobar/" class="nav-link px-2 text-white">Héctor Chocobar</a></li>
-      <li class="nav-item"><a href="https://www.linkedin.com/in/jrdelriod/" class="nav-link px-2 text-white">José Del Río</a></li>
+    <ul class="nav justify-content-center border-bottom pb-2 mb-2">
+      <li class="nav-item d-inline"><a href="https://www.linkedin.com/in/yuliya-bundur/" class="nav-link px-2 text-white">Yuliya Bundur<a href="https://github.com/yuliyabundur" target="_blank" rel="noopener noreferrer">
+        <img class="github-logo" src="GitHub-Logo.png" alt="GitHub" className="github-logo" />
+      </a></a>
+      </li> 
+      
+      <li class="nav-item"><a href="https://www.linkedin.com/in/carlos-duarte-rodr%C3%ADguez-bb2678174/" class="nav-link px-2 text-white">Carlos Duarte<a href="https://github.com/CarlosDuarteRod" target="_blank" rel="noopener noreferrer">
+        <img class="github-logo" src="GitHub-Logo.png" alt="GitHub" className="github-logo" />
+      </a></a>
+      </li>
+      
+      <li class="nav-item"><a href="#" class="nav-link px-2 text-white">Jorge Muñoz<a href="https://github.com/JorgeMunoz1991" target="_blank" rel="noopener noreferrer">
+        <img class="github-logo" src="GitHub-Logo.png" alt="GitHub" className="github-logo" />
+      </a></a>
+      </li>
+      
+      <li class="nav-item"><a href="https://www.linkedin.com/in/hector-chocobar/" class="nav-link px-2 text-white">Héctor Chocobar<a href="https://github.com/hchocobar" target="_blank" rel="noopener noreferrer">
+        <img class="github-logo" src="GitHub-Logo.png" alt="GitHub" className="github-logo" />
+      </a></a>
+      </li>
+      
+      <li class="nav-item">
+        <a href="https://www.linkedin.com/in/jrdelriod/" class="nav-link px-2 text-white">José Del Río<a href="https://github.com/jrdelrio" target="_blank" rel="noopener noreferrer">
+        <img class="github-logo" src="GitHub-Logo.png" alt="GitHub" className="github-logo" /></a>
+        </a>
+      </li>
       
     </ul>
         <p class="bottom-item text-center text-white">
